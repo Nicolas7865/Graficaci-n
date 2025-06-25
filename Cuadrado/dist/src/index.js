319b48a0813757da0ecfe95407bb0c8dc858bf9e
Nicolás_Orozco_Jimenez <97919376+Nicolas7865@users.noreply.github.com>
2025-06-25 07:09:24 -0600

    Delete Cuadrado directory

--- Cuadrado/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-import { CanvasCuadrado } from './canvasCuadrado.js';
-
-// Configurar el canvas para el cuadrado
-let canvasCuadrado = document.getElementById('circlechart2');
-let graphicsCuadrado = canvasCuadrado.getContext('2d');
-const miCanvasCuadrado = new CanvasCuadrado(graphicsCuadrado, canvasCuadrado);
-miCanvasCuadrado.paint();
